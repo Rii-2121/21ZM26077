9165eba1f200b466ee0f217cb591ec0ac8623d5f
Rii-2121 <zm26077@josai.ac.jp>
2026-06-29 10:43:41 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,1 +1,8 @@
-alert('Hello, Rii!')；
+alert('Hello, Rii!');
+alert(1+1);
+alert(Math.sqrt(1+1))
+alert(2 + 3);
+alert('2' + '3');
+alert(Number('2') + Number('3'));
+alert('2' + 3);
+alert(Number('2') + 3);
